cab6c250754ecd559d4dc4233413c33419924910
nyoloo <87368516+nyoloo@users.noreply.github.com>
2021-07-17 14:56:07 +0700

    fix side bar barista

--- view/dasboard/dasboard_barista.php
+++ view/dasboard/dasboard_barista.php
@@ -7,14 +7,6 @@ dbConnect();
 $data = getMenu()->fetch_all(MYSQLI_ASSOC);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="../../style.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-<body>
   <aside class="sidebar">
     <menu>
       <ul class="menu-content">
