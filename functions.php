61f3baf4ae9359776c052e61c0bb4b285cbf3a34
Faisal <klsfaisalobara@gmail.com>
2021-07-17 19:43:10 +0700

    fixed top navbar

--- functions.php
+++ functions.php
@@ -6,8 +6,9 @@ function dbConnect()
 }
 
 // Query Tambah Pesanan
-function addPesanan($noMeja, $statusPesanan ,$jumlahPesanan, $subTotal) {
-    return "INSERT INTO pesanan(no_meja, status_pesanan, jumlah_pesanan, sub_total) VALUES ('$noMeja', '$statusPesanan', '$jumlahPesanan', '$subTotal')";
+function addPesanan($noMeja, $statusPesanan, $jumlahPesanan, $subTotal)
+{
+  return "INSERT INTO pesanan(no_meja, status_pesanan, jumlah_pesanan, sub_total) VALUES ('$noMeja', '$statusPesanan', '$jumlahPesanan', '$subTotal')";
 }
 
 // Query Lihat Pesanan
@@ -50,10 +51,10 @@ function nav($title)
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../../style.css">
 
-      <title><?php echo $title ?></title>
+    <title><?php echo $title ?></title>
   </head>
 
-  <nav class="navbar navbar-dark" style="background-color: #293949">
+  <nav class="navbar navbar-dark sticky-top" style="background-color: #293949">
     <div class="container-fluid">
       <a class="navbar-brand">KopiHub</a>
       <!-- <h5 style="color: white;">Halo, <?= $_SESSION["nama"]; ?></h5> -->
